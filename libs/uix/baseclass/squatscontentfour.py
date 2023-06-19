016234d151c6dff65e82b9004edfffc370ff6a5a
AJFZap <ariel_zappia@hotmail.com>
2023-06-19 11:36:35 -0300

    Apply Lazy Loading

---
+++ libs/uix/baseclass/squatscontentfour.py
@@ -0,0 +1,5 @@
+from kivy.uix.screenmanager import Screen
+
+class SquatsContentFour(Screen):
+    pass
+
---
+++ libs/uix/baseclass/squatscontentsix.py
@@ -0,0 +1,5 @@
+from kivy.uix.screenmanager import Screen
+
+class SquatsContentSix(Screen):
+    pass
+
